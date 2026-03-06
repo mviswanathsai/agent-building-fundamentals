577c5b217d5ee2b3c1de5d570b55edec42a4e34c
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2026-03-06 14:04:19 +0530

    add a generic function call interface

---
+++ call_function.py
@@ -0,0 +1,21 @@
+from functions.config import available_tools_map
+from google.genai import types
+
+working_dir = "calculator"
+
+def call_function(function_name, function_args, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_name} with args {function_args}")
+    else:
+        print(f"Calling function: {function_name}")
+
+    result = ""
+    tool = available_tools_map.get(function_name)
+    result = tool(**function_args)
+
+    if result == "":
+        return f"Error: Unknown function: {function_name}"
+    else:
+        return  result
+
+
